7566009867a54b14ac91a65964b8581e05518bd6
Alfredo <icealfredo123@gmail.com>
2021-10-05 12:17:44 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Alfredo Gutierrez on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
