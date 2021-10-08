402c3ad23ebdf1b16741a70ac0d38f19f236a6c2
Alfredo <icealfredo123@gmail.com>
2021-10-08 08:21:37 -0500

    first commit

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Alfredo Gutierrez on 10/7/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
